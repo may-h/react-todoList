dea71496b471a97b319efaa191b68b2377938c82
may-h <hms9210@naver.com>
2020-07-01 16:41:58 +0900

    성능 최적화 적용

--- src/App.js
+++ src/App.js
@@ -3,62 +3,52 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기 ',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정관리 앱 만들기 ',
+//성능을 최적화 하기 위해서 React.memo를 사용하여 컴포넌트 성능을 최적화 시킬 수 있다. (TodoListItem.js)
+//즉, 컴포넌트의 props가 바뀌지 않았다면 리렌더링하지 않도록 설정 필요 --> React.memo()
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
+
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos); //createBulkTodos()라고 하면 리렌더링될 때마다 함수가 호출되지만, createBulkTodos 함수 형태로 넣어주면 컴포넌트가 처음 렌더링 될 때만 호출된다.
 
   //고윳값으로 사용될 id
   //ref 사용해서 변수 담기
   const nextId = useRef(4);
 
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text: text,
-        checked: false,
-      };
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text: text,
+      checked: false,
+    };
 
-      const nextTodos = todos.concat(todo);
-      setTodos(nextTodos);
-      nextId.current++;
-    },
-    [todos],
-  );
+    // useState의 함수형 업데이트로 성능 최적화
+    // const nextTodos = todos.concat(todo);
+    // setTodos(nextTodos);
+    setTodos((todos) => todos.concat(todo));
+    nextId.current++;
+  });
 
-  const onRemove = useCallback(
-    (id) => {
-      const nextTodos = todos.filter((todo) => todo.id !== id);
-      setTodos(nextTodos);
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  });
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  });
 
   return (
     <TodoTemplate>
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,19 +7,28 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove">
-        <MdRemoveCircleOutline onClick={() => onRemove(id)} />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove">
+          <MdRemoveCircleOutline onClick={() => onRemove(id)} />
+        </div>
       </div>
     </div>
   );
 };
 
-export default TodoListItem;
+//React.memo로 감싸주면 이제 TodoListItem 컴포넌트는 todo, onRemove,onToggle이 바뀌지 않으면 리렌더링하지 않는다.
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
